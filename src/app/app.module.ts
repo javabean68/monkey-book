bf81bd426be9c3c192d3881373ed5184a8826a9f
Fabio <safesfabio@gmail.com>
2022-01-30 21:04:45 +0100

    Refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
-import { SelectComponent } from './select/select.component';
-import { SelectBoxComponent } from './select-box/select-box.component';
 import { FormsModule } from '@angular/forms';
-import { CorrectionTextComponent } from './correction-text/correction-text.component';
+import { SelectComponent } from './jobs/widgets/select/select.component';
+import { SelectBoxComponent } from './jobs/widgets/select-box/select-box.component';
+import { CorrectionTextComponent } from './jobs/widgets/correction-text/correction-text.component';
 
 @NgModule({
   declarations: [
