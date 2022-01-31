c0489b715120189d3a7abe53c11aaf434ce08603
Fabio <safesfabio@gmail.com>
2022-01-31 15:48:29 +0100

    Weiterer Test angepasst

--- src/app/jobs/widgets/select-box/select-box.component.spec.ts
+++ src/app/jobs/widgets/select-box/select-box.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectBoxComponent } from './select-box.component';
+import { Clause } from '../shared/clause'
 
 describe('SelectBoxComponent', () => {
   let component: SelectBoxComponent;
@@ -21,5 +22,26 @@ describe('SelectBoxComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add a clause on add button click', () => {
+    {
+      //arrange
+      //act - simulate button click
+      component.addEmptyClause();
+      //assert - default clause + new
+      expect(component.clauses.length).toBe(2);
+    }
+  });
+
+  it('should update the internal clauses an event on "modelChange"', () => {
+    {
+      //arrange
+      //act - simulate text change
+      component.modelChange('DOSSIER_register_nr = value1 AND DOSSIER_register_nr !=  value2');
+
+      //assert
+      expect(component.clauses.length).toBe(2)
+    }
   });
 });
